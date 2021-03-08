29be9cef8b1238d0fbb2df0650df340df7142e5b
Yuriy Syrovetskiy <git-orbi@cblp.su>
2021-03-08 11:50:50 +0300

    Add stubs

--- NonEmpty.hs
+++ NonEmpty.hs
@@ -1,1 +1,25 @@
 module NonEmpty where
+
+import           Control.Applicative
+import           Data.Foldable
+import           Test.QuickCheck
+import           Test.QuickCheck.Poly
+
+data NonEmpty a = a :| [a]
+  deriving (Eq, Show)
+
+instance Arbitrary a => Arbitrary (NonEmpty a) where
+  arbitrary = liftA2 (:|) arbitrary arbitrary
+
+instance Functor NonEmpty where
+  fmap = undefined
+
+instance Foldable NonEmpty where
+  foldMap = undefined
+  foldr = undefined
+  toList = undefined
+
+prop_NonEmpty_fmaps_as_a_list :: Blind (A -> B) -> NonEmpty A -> Property
+prop_NonEmpty_fmaps_as_a_list (Blind _f) _xs =
+  -- toList (fmap f xs) === fmap f (toList xs)
+  property True
--- StringSum.hs
+++ StringSum.hs
@@ -1,1 +1,9 @@
 module StringSum where
+
+import           Test.QuickCheck
+
+stringSum :: String -> Maybe Int
+stringSum = undefined
+
+prop_stringSum_dummy :: Property
+prop_stringSum_dummy = "True" === "True"
